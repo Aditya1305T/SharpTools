b321a083f0a815ef9eedb89c6a1e0e8a5ddcf80c
Aditya1305T <adityatiwari1305@gmail.com>
2026-05-05 17:45:08 +0530

    edited file uplaod size

--- server/index.js
+++ server/index.js
@@ -34,8 +34,8 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 // ─── Routes ──────────────────────────────────────────────
 app.use('/api/auth',            require('./routes/auth'));
